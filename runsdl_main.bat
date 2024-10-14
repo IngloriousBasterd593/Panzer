6bf5da5cd1c97f5f5e1ddfbb01c3e0965f07c51d
IngloriousBasterd593 <adriansdacko@gmail.com>
2024-10-14 19:38:34 +0300

    fixed bat 2

--- runsdl_main.bat
+++ runsdl_main.bat
@@ -1,6 +1,6 @@
 @echo off
 mkdir "build/win" 2>NUL
-gcc main.c graphics.c -IC:\sdl2\include\SDL2 -LC:\sdl2\lib -g -ggdb -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_mixer -lSDL2_ttf -o build/win/gui
+gcc main.c graphics.c -IC:\sdl2\include\SDL2 -LC:\sdl2\lib -g -ggdb -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_mixer -lSDL2_ttf -o build/win/main
 cd "build/win"
-.\gui
+.\main
 
