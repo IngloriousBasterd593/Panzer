2b870b13d81dd051252fd57577ce73376458b90b
IngloriousBasterd593 <adriansdacko@gmail.com>
2024-12-10 07:44:32 +0200

    saglabat

---
+++ src/code/nonrelated/go/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	arr := []int{1, 2, 3, 4, 5} 
+	fmt.Println("Array elements:", arr)
+
+	var str string = "ILOVETOEAT"
+	fmt.Println(str)
+
+	byteArray := []byte{'H', 'e', 'l', 'l', 'o'}
+	str1 := string(byteArray)
+
+
+
+
+
+
+}
