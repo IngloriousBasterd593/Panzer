5e2395aa948112f7e5d551420984da034aae4404
MaoZedong666 <tavamatenosukaja22@gmail.com>
2024-12-16 15:51:56 +0200

    imzianas

--- src/code/nonrelated/goodbye.asm
+++ src/code/nonrelated/goodbye.asm
@@ -1,13 +1,11 @@
 section .data
-    helloworld db "Hello, World!", 0x0A    ; Define the string "Hello, World!\n"
+    helloworld db "Hello, World!", 0x0A    
 
 section .text
     global main
 
 main:
-    ; ================================
-    ; Step 1: sys_write
-    ; ================================
+
 
     mov rax, 1                  ; syscall number for sys_write (1)
     mov rdi, 1                  ; file descriptor 1 is stdout
@@ -15,9 +13,7 @@ main:
     mov rdx, 14                 ; hardcoded number of bytes to write (14)
     syscall                     ; invoke the syscall to write to stdout
 
-    ; ================================
-    ; Step 2: sys_exit
-    ; ================================
+
 
     mov rax, 60                 ; syscall number for sys_exit (60)
     xor rdi, rdi                ; set exit code to 0 (success)
